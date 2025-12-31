ed9e2b66bafb88184922318f0b81dfc626621d7e
ryo_pow <shennongliangtai@gmail.com>
2026-01-01 01:11:59 +0900

    websocket機能追加

--- main.py
+++ main.py
@@ -16,11 +16,26 @@ app = FastAPI()
 #Redis接続(decode_responses=Trueにすると文字列として取得できます)
 redis_client = redis.from_url("redis://localhost:6379", decode_responses=True)
 
+active_connections: list[WebSocket] =[]
+
+@app.websocket("/ws")
+async def connections_websocket(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
+
 @app.post("/whisper")
 async def create_whisper(message: str = ""):
     key = f"msg:{datetime.datetime.now().timestamp()}"
-    await redis_client.set(key,message,ex = 10)
-    return f"保存しました!"
+    await redis_client.set(key,message,ex = 3600)
+
+    for connection in active_connections:
+        await connection.send_text(f"新着メッセージ: {message}")
+    return "保存と送信が完了しました"
 
 @app.get("/whispers")
 async def get_whispers():
